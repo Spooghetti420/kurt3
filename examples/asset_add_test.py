d79fdead0246a7678004b25a9a04aa23f6b188c9
Spooghetti420 <kkovachev1947@gmail.com>
2022-03-15 14:46:11 +0000

    Update license, example structure

--- examples/asset_add_test.py
+++ examples/asset_add_test.py
@@ -1,13 +1,13 @@
 from kurt3.project import Project
 
 def main():
-    with Project("test/projects/src/Blank Project.sb3") as project:
+    with Project("projects/src/Blank Project.sb3") as project:
         sprite1 = project.get_sprite_by_name("Sprite1")
-        project.add_costume(sprite1, "test/projects/assets/Bread.svg", "Bread1")
-        project.add_costume(sprite1, "test/projects/assets/Bread.svg", "Bread2")
-        project.add_sound(sprite1, "test/projects/assets/A Bass.wav", "Bass1")
+        project.add_costume(sprite1, "assets/Bread.svg", "Bread1")
+        project.add_costume(sprite1, "assets/Bread.svg", "Bread2")
+        project.add_sound(sprite1, "assets/A Bass.wav", "Bass1")
         
-        project.save("test/projects/out/File Adding Test.sb3")
+        project.save("projects/out/File Adding Test.sb3")
 
 if __name__ == "__main__":
     main()
